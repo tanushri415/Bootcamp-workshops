f2e61430a23a971ff67ab30571854db39cd376b2
Tanushri Jain <jaintanushri34@gmail.com>
2023-09-27 03:54:09 -0500

    loops and arrays-only odds

---
+++ app.js
@@ -0,0 +1,44 @@
+// LOOPS AND ARRAYS // Only Odds
+
+const inputArr = [2, 4, 6, 8, 11, 20, 15, 22];
+const odds = [];
+
+for (let i = [0]; i <= inputArr.length - 1; i++) {
+  if (inputArr[i] % 2 != 0) {
+    odds.push(inputArr[i]);
+    console.log(odds);
+  }
+}
+// <------------------------------------------->
+
+const inputArr1 = [70, 42, 55, 81, 21, 91, 34];
+const odds1 = [];
+
+for (let i = [0]; i <= inputArr1.length - 1; i++) {
+  if (inputArr1[i] % 2 != 0) {
+    odds1.push(inputArr1[i]);
+    console.log(odds1);
+  }
+}
+// <---------------------------------------------->
+
+const inputArr2 = [2, 4, 6, 8, 10, 11, 12];
+const odds2 = [];
+
+for (let i = [0]; i <= inputArr2.length - 1; i++) {
+  if (inputArr2[i] % 2 != 0) {
+    odds2.push(inputArr2[i]);
+    console.log(odds2);
+  }
+}
+// <---------------------------------------------->
+
+const inputArr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const odds3 = [];
+
+for (let i = [0]; i <= inputArr3.length - 1; i++) {
+  if (inputArr3[i] % 2 != 0) {
+    odds3.push(inputArr3[i]);
+    console.log(odds3);
+  }
+}
